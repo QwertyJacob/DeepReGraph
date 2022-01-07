670516d4d8c54556d49d341972e4534d1d5fdea5
Jesus <jesusfernando.cevallosmoreno@jc.elis.org>
2022-01-07 21:36:08 +0100

    fix

--- adagae.py
+++ adagae.py
@@ -1052,7 +1052,7 @@ class AdaGAE():
         element_count = transposed_data_matrix.shape[1]
 
         if first_time:
-            self.S_Z = torch.zeros(element_count, element_count).to(self.device)
+            self.S_Z = torch.zeros(element_count, element_count)
         else:
             self.S_Z = self.compute_S_Z(transposed_Z=transposed_data_matrix)
 
@@ -1064,22 +1064,17 @@ class AdaGAE():
 
 
         self.S_D = self.compute_S_D()
-        print(self.S_D.device)
         self.S_G = torch.zeros(element_count, element_count)
         self.S_G[:self.ge_count,:self.ge_count] = self.CAN_precomputed_dist(self.D_G,self.current_gene_sparsity)
-        print(self.S_D.device)
 
         self.S_ATAC = torch.zeros(element_count, element_count)
         self.S_ATAC[self.ge_count:, self.ge_count:] = self.CAN_precomputed_dist(self.D_ATAC, self.current_sparsity)
-        print(self.S_D.device)
 
         self.S_ACET = torch.zeros(element_count, element_count)
         self.S_ACET[self.ge_count:, self.ge_count:] = self.CAN_precomputed_dist(self.D_ACET, self.current_sparsity)
-        print(self.S_D.device)
 
         self.S_METH = torch.zeros(element_count, element_count)
         self.S_METH[self.ge_count:, self.ge_count:] = self.CAN_precomputed_dist(self.D_METH, self.current_sparsity)
-        print(self.S_D.device)
 
 
         S = (self.S_Z * self.alpha_Z) + \
